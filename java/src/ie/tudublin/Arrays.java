af52741171a285caea804e1b09251498e4247dfb
Dylan <C22341463@mytudublin.ie>
2024-02-16 00:32:26 +0000

    Axis is labeled, but values appear to be wrong

--- java/src/ie/tudublin/Arrays.java
+++ java/src/ie/tudublin/Arrays.java
@@ -10,6 +10,11 @@ public class Arrays extends PApplet
 
 	float[] rainfall = {200, 260, 300, 150, 100, 50, 10, 40, 67, 160, 400, 420};
 
+	// For Bar Chart
+	int Num_Axis_Values = 12;
+
+	int maxIndex = 0;
+
 	public float map1(float a, float b, float c, float d, float e)
 	{
 		float r1 = c -b;
@@ -50,7 +55,6 @@ public class Arrays extends PApplet
 			}
 		}
 		
-		int maxIndex = 0;
 		for(int i= 0 ; i < rainfall.length ; i ++)
 		{
 			if (rainfall[i] > rainfall[maxIndex])
@@ -99,25 +103,45 @@ public class Arrays extends PApplet
 
 		
 		background(0);
-		stroke(255, 255, 255);
-
-		// Y Axis - 50 is 1.5 x 33.33
-		line(50, 50, 50, 450);
-		// X Axis
-		line(50, 450, 450, 450);
 
 		// Graph Title
 		text("Rainfall Bar Chart", 200, 25);
 
 		// There Are 2 50px Spaces On Either Side.
 		float w = (width - 100) / (float)months.length;
+		float Max_Y = 0;
+
 		for(int i = 0 ; i < months.length ;  i ++)
 		{
 			// Map X Values
 			float x = map1(i, 0, months.length, 50, (width - 50));
-			float y = map1(-rainfall[i], 0, -500, 0, -height + 100);
+			float y = map1(rainfall[i], 0, 500, 0, -height + 100);
 			stroke(0);
 			rect(x, height - 50, w, y);
+
+			// Y's Are Negative Cause Of Graph Orientation.
+			if (y < Max_Y)
+			{
+				Max_Y = y;
+			}
+
 		}
+
+		stroke(255, 255, 255);
+		// Y Axis - 50 is 1.5 x 33.33
+		line(50, 50, 50, 450);
+		// X Axis
+		line(50, 450, 450, 450);
+
+		// Calculate How Much We Got To Offset Values By
+		float offset = Max_Y / (float)Num_Axis_Values;
+		float h = 450 / Num_Axis_Values;
+
+		print("Max Y is : ", Max_Y);
+		for (int i = 0; i < Num_Axis_Values; i++)
+		{
+			text((int)-offset * i , 20, (height - 50) - i * h);	
+		}
+
 	}
 }
